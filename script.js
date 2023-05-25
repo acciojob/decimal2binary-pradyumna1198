1a64d5917af9f9452d1af4614b80fc73c36087f1
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-05-25 16:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function threeSum(arr, target) {
+function threeSum(num) {
 // write your code here
-  
+  var str ="";
+	while(num){
+		if(num&1)
+			str+='1';
+		else
+			str+='0';
+		num>>=1;//right shift
+	}
+	return str;
+}
+function rev(str){
+	var res="";
+	for(var i=str.length-1;i>=0;i--)
+		res+=(str[i]);
+	doucment.wite(res);
 }
 
 module.exports = threeSum;
