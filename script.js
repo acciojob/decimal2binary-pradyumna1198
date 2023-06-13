c1cbe6eb7d3fa117434e260dda3179cc273cc83c
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-06-13 18:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
 function threeSum(num) {
-// write your code here
- /* var str ="";
-	while(num){
-		if(num&1)
-			str+='1';
-		else
-			str+='0';
-		num>>=1;//right shift
-	}
-	return str;
-}
-function rev(str){
-	var res="";
-	for(var i=str.length-1;i>=0;i--)
-		res+=(str[i]);
-	doucment.wite(res);
-*/
-	let bnum = new Array(32);
-	let i=0;
-	while (n>0)
-		{
-			bnum[i]=n%2;
-			n=Math.floor(n/2);
-			i++;
-			}
-	for(let j=i-1;j>=0;j--)
-		return bnum[j];
-}
+	  // Check if the input is a non-negative integer
+  if (Number.isInteger(num) && num >= 0) {
+    // Special case for 0
+    if (num === 0) {
+      return '0';
+    }
 
-module.exports = threeSum;
+    // Perform the conversion
+    let binary = '';
+    while (num > 0) {
+      binary = (num % 2) + binary;
+      num = Math.floor(num / 2);
+    }
+
+    return binary;
+  } 
+}
