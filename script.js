a944daebb27974c1093bcd9044eacae0a3888c45
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:14:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-function threeSum(num) {
-	  let binaryNum = new Array(32);
- 
-    // counter for binary array
-    let i = 0;
-    while (n > 0) {
- 
-        // storing remainder in binary array
-        binaryNum[i] = n % 2;
-        n = Math.floor(n / 2);
-        i++;
-    }
- 
-    // printing binary array in reverse order
-    for (let j = i - 1; j >= 0; j--)
-        document.write(binaryNum[j]);
+function decimalToBinary(num) {
+  // Write your code here
 }
+
+window.decimalToBinary = decimalToBinary;
