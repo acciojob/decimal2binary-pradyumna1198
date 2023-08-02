b2d86991cc3f12dc7aea017e25ddab451a1ec631
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-08-02 13:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function decimalToBinary(num) {
   // Write your code here
+	let str=document.getElementById('rope-lengths').value.split(",");
+  let arr=str.map((str)=>parseInt(str));
+
+  
+let total=0;
+arr=arr.sort(compare);
+
+while(arr.length>=2){
+  let sum=arr[0]+arr[1];
+ 
+  let rem=[];
+  rem.push(sum);
+  for(let k=2;k<arr.length;k++){
+    rem.push(arr[k]);
+  }
+  rem=rem.sort(compare);
+ 
+  arr=[...rem];
+  
+  total=total+sum;
+  
+}
+console.log(total);
+ 
+ 
+let result=document.getElementById('result');
+result.innerHTML=total;
 }
 
 window.decimalToBinary = decimalToBinary;
